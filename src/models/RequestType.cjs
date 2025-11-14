5ddfd496f15b65730b5b37caaa528a298e46d8c9
Elouan Guillemoto <guillemotoelouan@gmail.com>
2025-11-14 19:49:00 +0100

    chore: add mongoose to dependencies

--- src/models/RequestType.cjs
+++ src/models/RequestType.cjs
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const RequestType = require('../models/RequestType.js');
+const RequestType = require('../models/RequestType.cjs');
 
 // GET /api/request-types
 router.get('/', async (req, res) => {
